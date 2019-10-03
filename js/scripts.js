6847b28313819ae9906039242177622f12515f39
David Boedigheimer <stealth259@gmail.com>
2019-10-03 15:55:22 -0700

    Add else if's and end result.

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,31 @@ $(document).ready(function() {
 
       $("#match").empty().append(result);
       $("#result").show();
-    } else if (age ) {
+    } else if (age >= 40 && gender === "male" && pet === "dog") {
+      var result = "kelly Rowan"
 
+      $("#match").empty().append(result);
+      $("#result").show();
+    } else if (age < 30 && pet === "dragon") {
+      var result = "Hermione Granger"
+
+      $("#match").empty().append(result);
+      $("#result").show();
+    } else if (age >= 20 && gender === "female" && pet === "dragon") {
+      var result = "Eragon Bromsson"
+
+      $("#match").empty().append(result);
+      $("#result").show();
+    } else if (age >= 18 && gender === "female" && pet === "cat") {
+      var result = "Shawn Mendes"
+
+      $("#match").empty().append(result);
+      $("#result").show();
+    } else {
+      var result = "Nobody. Sorry you are just not matchable with any Celebrity"
+
+      $("#match").empty().append(result);
+      $("#result").show();
     }
 
     event.preventDefault();
